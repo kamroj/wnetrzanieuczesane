c213efcdea50414f6d7c2a09d3e39642fb1d5352
Kamil Rojek <kamroj@gmail.com>
2024-06-07 23:19:22 +0200

    Add grid lines

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,17 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import ReactImageGallery from "react-image-gallery";
 import sanityClient from "../../SanityClient";
+import Loading from "../Loading/Loading";
+
 import logo from "../../assets/images/logo-white.png";
 import quote from "../../assets/images/quote.png";
-import Loading from "../Loading/Loading";
+import quoteBackground from "../../assets/images/scretch.png";
 
 import "./Home.scss";
 import "./Gallery.scss";
 import "./Quote.scss";
+import "./AboutCompany.scss";
+import "../GridLines.scss";
 
 const fetchSlides = async () => {
   const data = await sanityClient.fetch(
@@ -39,6 +43,11 @@ function Home() {
   return (
     <div className="home-container">
       <section className="gallery-container">
+        <div class="grid-lines grid-lines-vertical grid-lines-padding-menu">
+          <span class="g-line-vertical line-left"></span>
+          <span class="g-line-vertical line-center"></span>
+          <span class="g-line-vertical line-right"></span>
+        </div>
         <ReactImageGallery
           autoPlay={true}
           slideInterval={5000}
@@ -78,6 +87,18 @@ function Home() {
             src={quote}
             alt="quote-left"
             className="quote-image-container__quote-img quote-image-container__quote-img--right"
+          />
+        </div>
+      </section>
+      <section className="about-company-container">
+        <div className="about-company-content-container">
+          <h1>[ O FIRMIE ]</h1>
+        </div>
+        <div className="about-company-scretch-container">
+          <img
+            src={quoteBackground}
+            alt="quote-background"
+            className="about-company-scretch-container__img"
           />
         </div>
       </section>
