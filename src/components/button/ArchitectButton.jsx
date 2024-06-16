756f6e70283dea18e3a1af2ec60404b87a22debe
Kamil Rojek <kamroj@gmail.com>
2024-06-16 11:26:03 +0200

    Refactor ArchitectButton

--- src/components/button/ArchitectButton.jsx
+++ src/components/button/ArchitectButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./ArchitectButton.scss";
 
-export default function ArchitectButton({ name, onClick, isBlack }) {
+export default function ArchitectButton({ name, onClick, isBlack = false }) {
   return (
     <button
-      className={`architect-button ${isBlack && "black"}`}
+      className={`architect-button ${isBlack ? "black" : ""}`}
       onClick={onClick}
     >
       {name}
