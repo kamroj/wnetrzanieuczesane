ae892b3e6010b947b0d9221c6c3d4042980042bf
Kamil Rojek <kamroj@gmail.com>
2024-08-01 21:25:58 +0200

    Fix spinner position

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import './Loading.scss'
+// Loading.jsx
+import React from 'react';
+import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const Spinner = styled.div`
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: ${({ theme }) => theme.colors.golden};
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default function Loading() {
   return (
-    <div className="loading-container"><div className="loading"></div></div>
-  )
+    <LoadingContainer>
+      <Spinner />
+    </LoadingContainer>
+  );
 }
